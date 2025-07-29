d1cc65ac504d4835146d9042ba3274a6be965e45
Eduardojr06 <cm22004@esfe.agape.edu.sv>
2025-07-29 08:13:43 -0600

    Aaron

---
+++ demo/src/main/java/com/CitasClinicas/demo/CitasClinicasApplication.java
@@ -0,0 +1,13 @@
+package com.CitasClinicas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasClinicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasClinicasApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/CitasClinicas/demo/CitasClinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CitasClinicas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasClinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
