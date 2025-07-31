2f3104fb168bac3a6acca0b05b3742ac8745683e
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-07-31 11:43:02 -0600

    Modeficacion

--- demo/src/main/java/com/CitasClinicas/demo/Repositorios/IExpedienteClinicoRepositorio.java
+++ demo/src/main/java/com/CitasClinicas/demo/Repositorios/IExpedienteClinicoRepositorio.java
@@ -2,6 +2,6 @@ package com.CitasClinicas.demo.Repositorios;
 import com.CitasClinicas.demo.Modelos.ExpedienteClinico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// public interface IExpedienteClinicoRepositorio extends JpaRepository<ExpedienteClinico, Long> {
+public interface IExpedienteClinicoRepositorio extends JpaRepository<ExpedienteClinico, Long> {
 
-// }
+}
--- demo/src/main/java/com/CitasClinicas/demo/Repositorios/IMedicoRepositorio.java
+++ demo/src/main/java/com/CitasClinicas/demo/Repositorios/IMedicoRepositorio.java
@@ -2,6 +2,6 @@ package com.CitasClinicas.demo.Repositorios;
 import com.CitasClinicas.demo.Modelos.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// public interface IMedicoRepositorio extends JpaRepository<Medico, Long> {
+public interface IMedicoRepositorio extends JpaRepository<Medico, Long> {
 
-// }
+}
